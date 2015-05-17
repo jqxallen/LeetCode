1ac918f519abb3b90b415bed9ab9584c719a6535
Qiang Jia <jqxallen@gmail.com>
2015-05-17 23:39:53 +0800

    Pascal's Triangle

--- Algorithms/Pascal's Triangle/Source.cpp
+++ Algorithms/Pascal's Triangle/Source.cpp
@@ -1,3 +1,20 @@
+/*
+ * Author: Qiang Jia
+ * Date: Apr 19, 2015
+ * Link: https://leetcode.com/problems/pascals-triangle/
+ * Description:
+ * 		Given numRows, generate the first numRows of Pascal's triangle.
+ * 		For example, given numRows = 5,
+ * 		Return
+ *			[
+ *			     [1],
+ *			    [1,1],
+ *			   [1,2,1],
+ *			  [1,3,3,1],
+ *			 [1,4,6,4,1]
+ *			]
+ */
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
