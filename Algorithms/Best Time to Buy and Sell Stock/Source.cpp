ffcd13749420042b04faf745c0a87d0c8dcd8793
Qiang Jia <jqxallen@gmail.com>
2015-05-17 19:07:52 +0800

    Best Time to Buy and Sell Stock

--- Algorithms/Best Time to Buy and Sell Stock/Source.cpp
+++ Algorithms/Best Time to Buy and Sell Stock/Source.cpp
@@ -1,3 +1,12 @@
+/*
+ * Author: Qiang Jia
+ * Date: Nov 14, 2013
+ * Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * Description:
+ *		Say you have an array for which the ith element is the price of a given stock on day i.
+ *		If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ */
+
 #include <vector>
 using namespace::std;
 
