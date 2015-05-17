bdf4a5a65eebfc7d1c57272ca5bbc8a93381a90a
Qiang Jia <jqxallen@gmail.com>
2015-05-17 22:36:51 +0800

    Binary Tree Level Order Traversal

--- Algorithms/Binary Tree Level Order Traversal/Source.cpp
+++ Algorithms/Binary Tree Level Order Traversal/Source.cpp
@@ -1,3 +1,24 @@
+/*
+ * Author: Qiang Jia
+ * Date: Nov 16, 2013
+ * Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * Description:
+ * 		Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+ * 		For example:
+ * 			Given binary tree {3,9,20,#,#,15,7},
+ * 			    3
+ * 			   / \
+ *			   9  20
+ *			     /  \
+ *			    15   7
+ *			return its level order traversal as:
+ *			[
+ *				[3],
+ *				[9,20],
+ *				[15,7]
+ *			]
+ */
+
 #include <iostream>
 #include <vector>
 #include <queue>
