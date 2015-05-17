87b076f76e69d3a0fec2316600a7093316474fb7
Qiang Jia <jqxallen@gmail.com>
2015-05-17 15:34:23 +0800

    3Sum

--- 3Sum/Source.cpp
+++ 3Sum/Source.cpp
@@ -1,3 +1,18 @@
+/*
+ * Author: Qiang Jia
+ * Date: Nov 1, 2013
+ * Link: https://leetcode.com/problems/3sum/
+ * Description:
+ *		Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+ *		Note:
+ *			Elements in a triplet (a,b,c) must be in non-descending order. (ie, a <= b <= c)
+ *			The solution set must not contain duplicate triplets.
+ *			For example, given array S = {-1 0 1 2 -1 -4},
+ *			A solution set is:
+ *				(-1, 0, 1)
+ *				(-1, -1, 2)
+ */
+
 #include <iostream>
 #include <vector>
 #include <iterator>
@@ -19,34 +34,32 @@ public:
 					return;
 				}
 				coll.pop_back();
-			} else {
+			}
+			else {
 				find_numbers(num, N - 1, i + 1, end + 1, colls, coll);
 				coll.pop_back();
 			}
 		}
 	}
-    vector<vector<int> > threeSum(vector<int> &num) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        sort(num.begin(), num.end());
+	vector<vector<int> > threeSum(vector<int> &num) {
+		// IMPORTANT: Please reset any member data you declared, as
+		// the same Solution instance will be reused for each test case.
+		sort(num.begin(), num.end());
 		vector<vector<int>> colls;
 		vector<int> coll;
 		int N = 3;
 		find_numbers(num, N, 0, num.size() - N + 1, colls, coll);
 		return colls;
-    }
+	}
 };
 
 void main()
 {
-	int arr[] = {-1, 0, 1, 2, -1, -4};
+	int arr[] = { -1, 0, 1, 2, -1, -4 };
 	vector<int> num(arr, arr + sizeof(arr) / sizeof(int));
-
 	Solution solution;
-	for (const auto &coll: solution.threeSum(num)) {
+	for (const auto &coll : solution.threeSum(num)) {
 		copy(coll.cbegin(), coll.cend(), ostream_iterator<int>(cout, " "));
 		cout << endl;
 	}
-
-	system("pause");
 }
