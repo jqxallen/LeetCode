ce001796f54d7b7d4f0e588da41dacc90459c6d5
Qiang Jia <jqxallen@gmail.com>
2015-05-17 18:46:57 +0800

    Add Two Numbers

--- Algorithms/Add Two Numbers/Source.cpp
+++ Algorithms/Add Two Numbers/Source.cpp
@@ -1,4 +1,15 @@
+/*
+ * Author: Qiang Jia
+ * Date: Nov 1, 2013
+ * Link: https://leetcode.com/problems/add-two-numbers/
+ * Description:
+ *		You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *		Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ *		Output: 7 -> 0 -> 8
+ */
+
 #include <iostream>
+using namespace::std;
 
 struct ListNode {
 	int val;
@@ -16,18 +27,15 @@ struct ListNode {
  */
 class Solution {
 public:
-    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
-        if (!l1) return l2;
+	ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+		// IMPORTANT: Please reset any member data you declared, as
+		// the same Solution instance will be reused for each test case.
+		if (!l1) return l2;
 		if (!l2) return l1;
-
 		int carry = l1->val + l2->val;
 		ListNode * l = new ListNode(carry % 10);
 		carry /= 10;
-
-		ListNode * n = l, * n1 = l1->next, * n2 = l2->next;
-
+		ListNode * n = l, *n1 = l1->next, *n2 = l2->next;
 		while (n1 && n2) {
 			carry += n1->val + n2->val;
 			n->next = new ListNode(carry % 10);
@@ -36,7 +44,6 @@ public:
 			n1 = n1->next;
 			n2 = n2->next;
 		}
-
 		while (n1) {
 			carry += n1->val;
 			n->next = new ListNode(carry % 10);
@@ -44,7 +51,6 @@ public:
 			n = n->next;
 			n1 = n1->next;
 		}
-
 		while (n2) {
 			carry += n2->val;
 			n->next = new ListNode(carry % 10);
@@ -52,9 +58,7 @@ public:
 			n = n->next;
 			n2 = n2->next;
 		}
-
 		if (carry) n->next = new ListNode(carry);
-
 		return l;
-    }
+	}
 };
--- README.md
+++ README.md
@@ -9,3 +9,4 @@ LeetCode
 |18|[4Sum](https://leetcode.com/problems/4sum/)|[C++](./Algorithms/4Sum/Source.cpp)|Medium|Nov 13, 2013|
 |16|[3Sum Closest](https://leetcode.com/problems/3sum-closest/)|[C++](./Algorithms/3Sum Closest/Source.cpp)|Medium|Apr 17, 2015|
 |15|[3Sum](https://leetcode.com/problems/3sum/)|[C++](./Algorithms/3Sum/Source.cpp)|Medium|Nov 1, 2013|
+|2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)|[C++](./Algorithms/Add Two Numbers/Source.cpp)|Medium|Nov 1, 2013|
