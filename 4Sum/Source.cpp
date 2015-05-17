d608910135837b83ba6be1a887035233eb1f8b95
Qiang Jia <jqxallen@gmail.com>
2015-05-17 16:16:34 +0800

    4Sum

--- 4Sum/Source.cpp
+++ 4Sum/Source.cpp
@@ -1,3 +1,19 @@
+/*
+ * Author: Qiang Jia
+ * Date: Nov 13, 2013
+ * Link: https://leetcode.com/problems/4sum/
+ * Description:
+ *		Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+ *		Note:
+ *			Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a <= b <= c <= d)
+ *			The solution set must not contain duplicate quadruplets.
+ *			For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+ *			A solution set is:
+ *				(-1,  0, 0, 1)
+ *				(-2, -1, 1, 2)
+ *				(-2,  0, 0, 2)
+ */
+
 #include <vector>
 #include <set>
 #include <numeric>
@@ -13,7 +29,6 @@ public:
 		vector<vector<int>> colls;
 		int size = num.size();
 		if (size < 4) return colls;
-
 		sort(num.begin(), num.end());
 		set<vector<int>> temp_colls;
 		for (int i = 0; i < size; ++i) {
@@ -24,7 +39,7 @@ public:
 					if (sum > target) end--;
 					else if (sum < target) begin++;
 					else {
-						vector<int> coll = {num[i], num[j], num[begin], num[end]};
+						vector<int> coll = { num[i], num[j], num[begin], num[end] };
 						temp_colls.insert(coll);
 						begin++;
 						end--;
@@ -32,7 +47,6 @@ public:
 				}
 			}
 		}
-
 		copy(temp_colls.cbegin(), temp_colls.cend(), back_inserter<vector<vector<int>>>(colls));
 		return colls;
 	}
