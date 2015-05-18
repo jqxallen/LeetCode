b05db19f87151bf8823f5e162a456ab665c7c095
Qiang Jia <jqxallen@gmail.com>
2015-05-18 21:52:42 +0800

    Two Sum

--- Algorithms/Two Sum/Source.cpp
+++ Algorithms/Two Sum/Source.cpp
@@ -1,3 +1,15 @@
+/*
+ * Author: Qiang Jia
+ * Date: Nov 1, 2013
+ * Link: https://leetcode.com/problems/two-sum/
+ * Description:
+ *		Given an array of integers, find two numbers such that they add up to a specific target number.
+ *		The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
+ *		You may assume that each input would have exactly one solution.
+ *		Input: numbers={2, 7, 11, 15}, target=9
+ *		Output: index1=1, index2=2
+ */
+
 #include <iostream>
 #include <vector>
 #include <map>
@@ -7,9 +19,9 @@ using namespace::std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-        // IMPORTANT: Please reset any member data you declared, as
-        // the same Solution instance will be reused for each test case.
+	vector<int> twoSum(vector<int> &numbers, int target) {
+		// IMPORTANT: Please reset any member data you declared, as
+		// the same Solution instance will be reused for each test case.
 		multimap<int, int> coll;
 		for (int i = 0; i < numbers.size(); ++i)
 			coll.insert(make_pair(numbers[i], i + 1));
@@ -25,19 +37,15 @@ public:
 		index.push_back(last->second);
 		sort(index.begin(), index.end());
 		return index;
-    }
+	}
 };
 
 void main()
 {
-	int arr[] = {150,24,79,50,88,345,3};
+	int arr[] = { 150, 24, 79, 50, 88, 345, 3 };
 	int target = 200;
-
 	vector<int> numbers(arr, arr + sizeof(arr) / sizeof(int));
-
 	Solution solution;
 	vector<int> res = solution.twoSum(numbers, target);
 	copy(res.cbegin(), res.cend(), ostream_iterator<int>(cout, "\n"));
-
-	system("pause");
 }
--- README.md
+++ README.md
@@ -30,3 +30,4 @@ LeetCode
 |16|[3Sum Closest](https://leetcode.com/problems/3sum-closest/)|[C++](./Algorithms/3Sum Closest/Source.cpp)|Medium|Apr 17, 2015|
 |15|[3Sum](https://leetcode.com/problems/3sum/)|[C++](./Algorithms/3Sum/Source.cpp)|Medium|Nov 1, 2013|
 |2|[Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)|[C++](./Algorithms/Add Two Numbers/Source.cpp)|Medium|Nov 1, 2013|
+|1|[Two Sum](https://leetcode.com/problems/two-sum/)|[C++](./Algorithms/Two Sum/Source.cpp)|Medium|Nov 1, 2013|
