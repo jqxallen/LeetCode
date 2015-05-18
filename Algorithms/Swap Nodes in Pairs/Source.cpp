82ad13b6bc3bd87256fe27c102476043c8611ed7
Qiang Jia <jqxallen@gmail.com>
2015-05-18 22:22:34 +0800

    Swap Nodes in Pairs

--- Algorithms/Swap Nodes in Pairs/Source.cpp
+++ Algorithms/Swap Nodes in Pairs/Source.cpp
@@ -1,3 +1,14 @@
+/*
+ * Author: Qiang Jia
+ * Date: Nov 21, 2013
+ * Link: https://leetcode.com/problems/swap-nodes-in-pairs/
+ * Description:
+ *		Given a linked list, swap every two adjacent nodes and return its head.
+ *		For example,
+ *			Given 1->2->3->4, you should return the list as 2->1->4->3.
+ *			Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+ */
+
 #include <iostream>
 
 struct ListNode {
